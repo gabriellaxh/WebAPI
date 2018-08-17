a224710a4604bc5b509a9b6d5a68e5f178913808
Gabriella Shirkova <gabriellashirkova@gmail.com>
2018-08-17 08:31:07 +0300

    Homework

---
+++ WebAPI/Homework/Controllers/InitialController.cs
@@ -0,0 +1,53 @@
+﻿using Homework.Models;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace Homework.Controllers
+{
+    public class InitialController : ApiController
+    {
+        // GET: Api
+
+        static List<CustomerModel> customers = new List<CustomerModel>
+        {
+            new CustomerModel
+            {
+                Id = 2,
+                Firstname = "Ivan",
+                Lastname = "Petrov",
+                PhoneNumber = "0891234865",
+                SecondPhoneNumber = "0286345",
+                Address = "ul. Momina salza 15"
+            },
+            new CustomerModel
+            {
+                Id= 4,
+                Firstname = "Simona",
+                Lastname = "Dimitrova",
+                PhoneNumber = "0886345912",
+                SecondPhoneNumber = "0234568",
+                Address = "ul. Shipchenska"
+            },
+            new CustomerModel
+            {
+                Id= 8,
+                Firstname = "Hristo",
+                Lastname = "Antonov",
+                PhoneNumber = "0886652315",
+                SecondPhoneNumber = "0233225",
+                Address = "ul. Sveta gora"
+            }
+        };
+
+        public CustomerModel Get(int id)
+        {
+            var customerWanted = customers.Find(x => x.Id == id);
+
+            return customerWanted; 
+            
+        }
+    }
+}
---
+++ WebAPI/Homework/Models/CustomerModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Homework.Models
+{
+    public class CustomerModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100, ErrorMessage = "Firstname is too long!")]
+        public string Firstname { get; set; }
+
+        [Required]
+        [MaxLength(100, ErrorMessage = "Lastname is too long!")]
+        public string Lastname { get; set; }
+
+        [Required]
+        [MaxLength(100, ErrorMessage = "Phone Number is too long!")]
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [MaxLength(100, ErrorMessage = "Second Phone Number is too long!")]
+        [Display(Name = "Second Phone Number")]
+        public string SecondPhoneNumber { get; set; }
+
+        [Required]
+        [MaxLength(100, ErrorMessage = "Address is too long!")]
+        public string Address { get; set; }
+    }
+}
